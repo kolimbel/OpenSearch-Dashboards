1ac6e4b2393677e9930e320cc7fa8a026b6f647b
mateusz.janusz <mateusz.janusz@trapezegroup.pl>
2025-02-04 11:26:00 +0100

    build: add font base64 transformer

---
+++ scripts/generate_font_base64.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+var FONT_PATH = path.join(__dirname, '../assets/Roboto-Regular.ttf');
+var OUTPUT_PATH = path.join(__dirname, '../src/plugins/pdf_export/fonts/font_base64.ts');
+
+if (!fs.existsSync(FONT_PATH)) {
+  console.error(`❌ Font file not found: ${FONT_PATH}`);
+  process.exit(1);
+}
+
+var fontBuffer = fs.readFileSync(FONT_PATH);
+var base64Font = fontBuffer.toString('base64');
+
+var outputContent = `export const fontBase64 = "${base64Font}";\n`;
+
+fs.writeFileSync(OUTPUT_PATH, outputContent, 'utf8');
+
+console.log(`✅ Base64 font saved to: ${OUTPUT_PATH}`);
